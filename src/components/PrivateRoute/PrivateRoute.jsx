b1914d7f9634f7856e4c4c18a5126a6ef884f39a
Nafiz Anam <nafiz.anam002@gmail.com>
2021-10-20 23:15:55 +0600

    bug fix

--- src/components/PrivateRoute/PrivateRoute.jsx
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 import { Redirect, Route } from "react-router";
 import useAuth from "../../Hooks/useAuth";
-import {Spinner} from 'react-bootstrap'
+import { Spinner } from "react-bootstrap";
 
 const PrivateRoute = ({ children, ...rest }) => {
     const { user, isLoading } = useAuth();
 
-    if(isLoading){
-        return <Spinner animation="border" variant="danger" />
+    if (isLoading) {
+        return <Spinner animation="border" variant="danger" />;
     }
-    
+
     return (
         <Route
             {...rest}
             render={({ location }) =>
-                user.email ? (
+                user?.email ? (
                     children
                 ) : (
                     <Redirect
